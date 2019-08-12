0961e0d67cf182f7722bbd3922889ff06140dde6
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-08-12 09:24:19 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,18 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sum(a, b) {
+    if(b === "sheep"){
+        return false;
+    }
+    return a + b;
+}
+
+function isEven(input) {
+    if(input > 3){
+        return true;
+    }else {
+        return false;
+    }
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,33 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sum', function () {
+   it('should be a defined function', function () {
+       expect(typeof sum).toBe('function');
+    });
+   it('should return the number 4 when passed the numbers 2 and 2', function () {
+       expect(sum(2, 2)).toBe(4)
+   });
+    it('should return the number 5 when passed the numbers 2 and 3', function () {
+        expect(sum(2, 3)).toBe(5)
+    });
+    it('should return false when passed two strings', function () {
+        expect(sum('black', 'sheep')).toBe(false)
+    });
+});
+
+describe('isEven', function () {
+   it('should be a defined function', function () {
+       expect(typeof isEven).toBe('function');
+   });
+    it('should be a boolean', function () {
+        expect(typeof isEven()).toBe('boolean')
+    });
+    it('should return true on 2', function () {
+       expect(isEven(2)).toBe(true);
+    });
+    it('should return true on -4', function () {
+        expect(isEven(-4)).toBe(true);
+    });
+});
